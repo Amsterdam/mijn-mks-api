b51f19c4f5147484cfa3b3cf80cff8f722ce0184
Johan Otten <drakonen@drakonen.com>
2020-01-06 16:43:46 +0100

    Added lookup dict for geheimhouding

--- mks/model/gba.py
+++ mks/model/gba.py
@@ -12,6 +12,19 @@ lookup_geslacht = {
     'V': 'Vrouw',
 }
 
+# source: wsdl/bg0310_msg_vraagAntwoord.xsd
+lookup_geheimhouding = {
+    "0": "geen beperking",
+    "1": "niet zonder toestemming aan derden ter uitvoering van een algemeen verbindend voorschrift",
+    "2": "niet aan kerken",
+    "3": "niet aan vrije derden",
+    "4": "niet zonder toestemming aan derden ter uitvoering van een algemeen verbindend voorschrift en niet aan kerken",
+    "5": "niet zonder toestemming aan derden ter uitvoering van een algemeen verbindend voorschrift en niet aan vrije derden",
+    "6": "niet aan kerken en niet aan vrije derden",
+    "7": "niet zonder toestemming aan derden ter uitvoering van een algemeen verbindend voorschrift en niet aan vrije derden en niet aan kerken",
+}
+
+
 # Source:
 # https://publicaties.rvig.nl/Landelijke_tabellen/Landelijke_tabellen_32_t_m_60_excl_tabel_35/Landelijke_Tabellen_32_t_m_60_in_csv_formaat
 
