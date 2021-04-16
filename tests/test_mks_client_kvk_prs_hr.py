3e61a3b82f56a8a8e3104ccd42d12eb7a0765873
Tim van Oostrom <tim@depulz.nl>
2021-04-16 10:24:05 +0200

    Refactor HR kvk response
    Signed-off-by: Tim van Oostrom <tim@depulz.nl>

--- tests/test_mks_client_kvk_prs_hr.py
+++ tests/test_mks_client_kvk_prs_hr.py
@@ -9,6 +9,70 @@ FIXTURE_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'fixture
 RESPONSE_PATH = os.path.join(FIXTURE_PATH, "hr_kvk_prs_response.xml")
 RESPONSE_EMPTY_PATH = os.path.join(FIXTURE_PATH, "hr_empty_response.xml")
 
+KVK_HR_PRS_RESPONSE = {
+    'mokum': True,
+    'nnpid': None,
+    'onderneming': {
+        'datumAanvang': date(1992, 1, 1),
+        'datumEinde': date(2020, 1, 1),
+        'handelsnamen': ['Naam 1', 'Naam 2', 'Naam 3', 'Naam 4'],
+        'hoofdactiviteit': 'Overige administratiekantoren',
+        'overigeActiviteiten': ['Arbeidsbemiddeling', 'Organisatie-adviesbureaus'],
+        'rechtsvorm': 'Eenmanszaak'
+    },
+    'eigenaar': {
+        'naam': 'Voornaam Achternaam',
+                'geboortedatum': date(1970, 1, 1),
+                'adres': {
+                    'huisletter': None,
+                    'huisnummer': '1',
+                    'huisnummertoevoeging': None,
+                    'postcode': '1012 NP',
+                    'straatnaam': 'Amstel',
+                    'woonplaatsNaam': 'Amsterdam'
+                },
+    },
+    'rechtspersonen': [
+        {
+            'bsn': '999999999',
+            'kvkNummer': '012345678',
+            'rsin': None,
+            'statutaireNaam': None,
+            'statutaireZetel': None
+        }
+    ],
+    'vestigingen': [
+        {
+            'activiteiten': ['Overige administratiekantoren', 'Organisatie-adviesbureaus', 'Arbeidsbemiddeling'],
+            'bezoekadres': {
+                'huisletter': None,
+                'huisnummer': '1',
+                'huisnummertoevoeging': None,
+                'postcode': '1012 PN',
+                'straatnaam': 'Amstel',
+                'woonplaatsNaam': 'Amsterdam'
+            },
+            'datumAanvang': date(1992, 1, 1),
+            'datumEinde': date(2020, 1, 1),
+            'emailadres': None,
+            'faxnummer': None,
+            'handelsnamen': ['Naam 1', 'Naam 2', 'Naam 3', 'Naam 4'],
+            'postadres': {
+                'huisletter': None,
+                'huisnummer': '1',
+                'huisnummertoevoeging': None,
+                'postcode': '1012 PN',
+                'straatnaam': 'Amstel',
+                'woonplaatsNaam': 'Amsterdam'
+            },
+            'telefoonnummer': None,
+            'typeringVestiging': 'Hoofdvestiging',
+            'vestigingsNummer': '000000000001',
+            'websites': []
+        }
+    ],
+}
+
 
 def get_xml_response_fixture(*args):
     with open(RESPONSE_PATH, 'rb') as response_file:
@@ -22,76 +86,11 @@ def get_xml_response_empty_fixture(*args):
 
 class KvkHrTest(TestCase):
 
-    def _expected_result(self) -> dict:
-        return {
-            'mokum': True,
-            'nnpid': None,
-            'onderneming': {
-                'datumAanvang': date(1992, 1, 1),
-                'datumEinde': date(2020, 1, 1),
-                'handelsnamen': ['Naam 1', 'Naam 2', 'Naam 3', 'Naam 4'],
-                'hoofdactiviteit': 'Overige administratiekantoren',
-                'overigeActiviteiten': ['Arbeidsbemiddeling', 'Organisatie-adviesbureaus'],
-                'rechtsvorm': 'Eenmanszaak'
-            },
-            'eigenaar': {
-                'naam': 'Voornaam Achternaam',
-                'geboortedatum': date(1970, 1, 1),
-                'adres': {
-                    'huisletter': None,
-                    'huisnummer': '1',
-                    'huisnummertoevoeging': None,
-                    'postcode': '1012 NP',
-                    'straatnaam': 'Amstel',
-                    'woonplaatsNaam': 'Amsterdam'
-                },
-            },
-            'rechtspersonen': [
-                {
-                    'bsn': '999999999',
-                    'kvkNummer': '012345678',
-                    'rsin': None,
-                    'statutaireNaam': None,
-                    'statutaireZetel': None
-                }
-            ],
-            'vestigingen': [
-                {
-                    'activiteiten': ['Overige administratiekantoren', 'Organisatie-adviesbureaus', 'Arbeidsbemiddeling'],
-                    'bezoekadres': {
-                        'huisletter': None,
-                        'huisnummer': '1',
-                        'huisnummertoevoeging': None,
-                        'postcode': '1012 PN',
-                        'straatnaam': 'Amstel',
-                        'woonplaatsNaam': 'Amsterdam'
-                    },
-                    'datumAanvang': date(1992, 1, 1),
-                    'datumEinde': date(2020, 1, 1),
-                    'emailadres': None,
-                    'faxnummer': None,
-                    'handelsnamen': ['Naam 1', 'Naam 2', 'Naam 3', 'Naam 4'],
-                    'postadres': {
-                        'huisletter': None,
-                        'huisnummer': '1',
-                        'huisnummertoevoeging': None,
-                        'postcode': '1012 PN',
-                        'straatnaam': 'Amstel',
-                        'woonplaatsNaam': 'Amsterdam'
-                    },
-                    'telefoonnummer': None,
-                    'typeringVestiging': 'Hoofdvestiging',
-                    'vestigingsNummer': '000000000001',
-                    'websites': []
-                }
-            ],
-        }
-
     @patch('mks.service.mks_client_bsn_hr._get_response', get_xml_response_fixture)
     def test_get(self):
         result = mks_client_bsn_hr.get_from_kvk('123456789')
 
-        self.assertEqual(result, self._expected_result())
+        self.assertEqual(result, KVK_HR_PRS_RESPONSE)
 
     @patch('mks.service.mks_client_bsn_hr._get_response', get_xml_response_empty_fixture)
     def test_get_empty(self):
