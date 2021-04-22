981a041a31f10106aabee2c97a918532cc14b925
Tim van Oostrom <tim@depulz.nl>
2021-04-22 09:30:34 +0200

    Update tests  swagger
    Signed-off-by: Tim van Oostrom <tim@depulz.nl>

--- tests/test_server.py
+++ tests/test_server.py
@@ -2,13 +2,14 @@ import json
 from unittest.mock import patch
 
 from mks.server import application
-from tma_saml import FlaskServerTMATestCase
 from tma_saml.for_tests.cert_and_key import server_crt
-
+from flask_testing.utils import TestCase
 from .test_mks_client_bsn_hr import BSN_HR_RESPONSE, get_bsn_xml_response_fixture
 from .test_mks_client_kvk_prs_hr import KVK_HR_PRS_RESPONSE, get_kvk_prs_xml_response_fixture, get_xml_response_empty_fixture
 from .test_mks_client_kvk_mac_hr import KVK_HR_MAC_RESPONSE, get_kvk_mac_xml_response_fixture
 from .test_mks_client_nnp_hr import NNP_HR_RESPONSE, get_nnp_xml_response_fixture
+from tma_saml import FlaskServerTMATestCase
+# from tma_saml.for_tests.fixtures import generate_saml_token_for_kvk, generate_saml_token_for_bsn
 
 
 def wrap_response(response_data, status: str = 'OK'):
@@ -18,9 +19,15 @@ def wrap_response(response_data, status: str = 'OK'):
     }
 
 
-class HRTest(FlaskServerTMATestCase):
+class HRTest(FlaskServerTMATestCase, TestCase):
+
+    def create_app(self):
+
+        application.config['TESTING'] = True
+        application.testing = True
+        return application
+
     def setUp(self) -> None:
-        self.client = self.get_tma_test_app(application)
         self.maxDiff = None
 
 
@@ -34,6 +41,7 @@ class HrBsnTest(HRTest):
     def test_bsn(self):
         headers = self.add_digi_d_headers('999999990')
         response = self.client.get('/brp/hr', headers=headers)
+
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.json, self._get_expected())
 
@@ -89,10 +97,11 @@ class HrKvkMacTest(HRTest):
     def _get_expected(self):
         return wrap_response({**KVK_HR_MAC_RESPONSE, **NNP_HR_RESPONSE})
 
-    @ patch('mks.service.mks_client_hr._get_response_by_kvk_number', lambda kvk_number: get_kvk_mac_xml_response_fixture())
-    @ patch('mks.service.mks_client_hr._get_response_by_nnpid', lambda kvk_number: get_nnp_xml_response_fixture())
+    @patch('mks.service.mks_client_hr._get_response_by_kvk_number', lambda kvk_number: get_kvk_mac_xml_response_fixture())
+    @patch('mks.service.mks_client_hr._get_response_by_nnpid', lambda kvk_number: get_nnp_xml_response_fixture())
     def test_get_mac_hr(self):
         headers = self.add_e_herkenning_headers('999999990')
         response = self.client.get('/brp/hr', headers=headers)
+        print(response)
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.json, self._get_expected())
