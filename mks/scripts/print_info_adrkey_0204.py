df537288ccbd64277adc51258f34e315a0343d46
Tim van Oostrom <tim@depulz.nl>
2021-07-01 11:26:28 +0200

    Refactor scripts for clarity and ease
    Signed-off-by: Tim van Oostrom <tim@depulz.nl>

--- mks/scripts/print_info_adrkey_0204.py
+++ mks/scripts/print_info_adrkey_0204.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import json
+from sys import argv
 
 import mks.service.adr_mks_client_02_04 as mks_client
 from mks.model.stuf_utils import decrypt  # noqa: F401
@@ -8,9 +9,11 @@ from mks.model.stuf_utils import decrypt  # noqa: F401
 # Turn on dumping of raw soap xml
 mks_client.log_response = True
 
-# Change me to get the datas
-adres_sleutel = "123456789"
+adres_sleutel = argv[1]
 # adres_sleutel = decrypt("")
+
+if not adres_sleutel:
+    exit("Adres sleutel not provided")
 
 data = mks_client.get(adres_sleutel)
 print(json.dumps(data, indent=2, default=str))
