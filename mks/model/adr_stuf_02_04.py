e5f5ebfd874da61a881d4ef881e447a54e8e7eba
Tim van Oostrom <tim@depulz.nl>
2021-07-26 10:29:58 +0200

    Remove redundant error logging
    Signed-off-by: Tim van Oostrom <tim@depulz.nl>

--- mks/model/adr_stuf_02_04.py
+++ mks/model/adr_stuf_02_04.py
@@ -4,7 +4,6 @@ from datetime import datetime
 from bs4 import Tag
 
 from mks.model.stuf_utils import to_datetime, is_nil
-from mks.service.exceptions import NoResultException
 
 
 def extract_data(adr_tree: Tag):
@@ -16,11 +15,11 @@ def extract_data(adr_tree: Tag):
 
     if is_nil(residents_data):
         logging.error("No data for address")
-        raise NoResultException()
+        return None
 
     for res in residents_data:
-        tijdvak = res.find('tijdvakRelatie', recursive=False)
-        endDate = to_datetime(tijdvak.find('einddatumRelatie', recursive=False))
+        tijdvak = res.find("tijdvakRelatie", recursive=False)
+        endDate = to_datetime(tijdvak.find("einddatumRelatie", recursive=False))
 
         if endDate and endDate > now:
             continue
@@ -28,5 +27,5 @@ def extract_data(adr_tree: Tag):
         resident_count += 1
 
     return {
-        'residentCount': resident_count,
+        "residentCount": resident_count,
     }
