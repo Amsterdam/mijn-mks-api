4f1f0e8eb64c61dcc91b4b218830a054bd369324
Tim van Oostrom <t.van.oostrom@amsterdam.nl>
2025-04-16 15:41:41 +0200

    Add SoapAction (#136)

--- scripts/print_info_adrkey_0204.py
+++ scripts/print_info_adrkey_0204.py
@@ -4,14 +4,11 @@ import json
 from sys import argv
 
 import app.service.adr_mks_client_02_04 as mks_client
-from app.helpers import decrypt  # noqa: F401
-
 
 adres_sleutel = argv[1]
-# adres_sleutel = decrypt("")
 
 if not adres_sleutel:
     exit("Adres sleutel not provided")
 
-data = mks_client.get(adres_sleutel)
+data = mks_client.get_resident_count(adres_sleutel)
 print(json.dumps(data, indent=2, default=str))
